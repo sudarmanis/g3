ac89aec75fb258212e8e9d6a1405f634a7b577b5
sudarmanis <41697795+sudarmanis@users.noreply.github.com>
2019-08-20 06:17:02 -0700

    Update max.java

--- max.java
+++ max.java
@@ -5,14 +5,15 @@ public static void main(String argS[])
 {
 int i,a;
 Scanner sc=new Scanner(System.in);
-System.out.println("enter the no");
+System.out.println("enter the 
+		   no");
 a=sc.nextInt();
 int n []=new int[a];
 for(i=0;i<a;i++)
 {
 n[i]=sc.nextInt();
 }
-int b[]=new int[n.length];
+
 int max=n[0];
 for(i=1;i<n.length;i++)
 {
