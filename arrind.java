ec66faf862aa045ebaa1be379e9206ac6e489651
sudarmanis <41697795+sudarmanis@users.noreply.github.com>
2019-08-20 07:01:18 -0700

    Create arrind.java

---
+++ arrind.java
@@ -0,0 +1,35 @@
+
+import java.util.*;
+class arrind
+{
+public static void main(String argS[])
+{
+int i,a,t=0,j;
+Scanner sc=new Scanner(System.in);
+
+a=sc.nextInt();
+int n []=new int[a];
+for(i=0;i<a;i++)
+{
+n[i]=sc.nextInt();
+}
+for(i=0;i<n.length;i++)
+{
+	for( j=1;j<n.length;j++)
+	{
+		if(n[i]>n[j])
+		{
+			t=n[i];
+			n[i]=n[j];
+			n[j]=t;
+		}
+	}
+}
+
+System.out.println();
+for(i=0;i<n.length;i++)
+{
+System.out.println(n[i]+" "+i);
+}
+}}
+
